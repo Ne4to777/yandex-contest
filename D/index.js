ed54ab46054624ec75755717344f2963ff81ace5
Alexey Alexeyev <ne4to777@gmail.com>
2020-04-05 15:43:10 +0300

    D task

--- D/index.js
+++ D/index.js
@@ -5,29 +5,27 @@ const rl = require('readline').createInterface({
 })
 
 const buffer = Buffer.alloc(7000000)
+let position = 0
 
 const read = line => {
-	const power = parseInt(line, 0)
-
-
-
-	// fs.writeFileSync(`${__dirname}/output.txt`, power)
+	generateR('', 0, 0, parseInt(line, 0))
 }
 
-const generate = (cur, opened, closed, n) => {
-	if (cur.length == 2 * n) {
-		print(cur)
-
+const generateR = (cur, opened, closed, n) => {
+	const length = 2 * n
+	if (cur.length === length) {
+		buffer.write(`${cur}\n`, position)
+		position += length + 1
 	} else {
-
+		if (opened < n) generateR(`${cur}(`, opened + 1, closed, n)
+		if (closed < opened) generateR(`${cur})`, opened, closed + 1, n)
 	}
-	return
-	if opened < n:
-		generate(cur + '(', opened + 1, closed, n)
-	if closed < opened:
-		generate(cur + ')', opened, closed + 1, n)
+}
 
+const close = () => {
+	fs.writeFileSync(`${__dirname}/output.txt`, buffer.slice(0, buffer.indexOf(0x00)))
 }
 
 rl
 	.on('line', read)
+	.on('close', close)
